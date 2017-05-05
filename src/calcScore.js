b168bc8f428c2f90c7725072f9ed948fb5d34062
Elin Blomgren <elinblomgren@gmail.com>
2017-05-05 17:21:44 +0200

    First try for a real comparison algorithm

--- src/calcScore.js
+++ src/calcScore.js
@@ -1,13 +1,23 @@
 import {alternatives, aspects} from "./sampleData";
 const calcScore = (id) => {
-  console.log(id);
+  id = id-1;
+  console.log("id: " + id);
   let score = 0;
-  for (let a of aspects) {
-    const value = alternatives[id-1].values[a.id];
-    console.log(a);
-    if (a.weight) {
-        score +=  value * a.weight;
-        console.log(score);
+  for (let aspect of aspects) {
+    let values = [];
+    if (aspect.weight) {
+      for (let alternative of alternatives)
+        values.push(alternative.values[aspect.id]);
+      let relativeValue = 0;
+      const min = Math.min(...values);
+      const max = Math.max(...values);
+      if (aspect.best === "max") {
+        relativeValue = (values[id]) / max;
+      } else if (aspect.best === "min") {
+        relativeValue = min/(values[id]);
+      }
+      console.log(aspect.id + relativeValue*aspect.weight);
+      score += relativeValue*aspect.weight;
     }
   }
   return score;
