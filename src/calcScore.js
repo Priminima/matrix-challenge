e87d1d17a97ce9e1e8d198cff602f07f0689aa15
Elin Blomgren <elinblomgren@gmail.com>
2017-05-07 19:42:57 +0200

    Move relative value calculation to own function

--- src/calcScore.js
+++ src/calcScore.js
@@ -1,26 +1,43 @@
 import {alternatives, aspects} from "./sampleData";
+
+/** Calculates a percent that represents how far the
+ * value are from the best value in values
+ *
+ * @values an array of numbers
+ * @best a string that is either 'max' or 'min'
+ * @value the value that you want to convert to a relative value
+ * */
+const calcRelativeValue = (values, best, value) => {
+  let relativeValue = 0;
+  const min = Math.min(...values);
+  const max = Math.max(...values);
+  if (best === "max") {
+    relativeValue = value / max;
+  } else if (best === "min") {
+    relativeValue = min / value;
+  }
+  return relativeValue;
+};
+
+/** Calculates a score for a product
+ * @id the id of the product
+ * */
 const calcScore = (id) => {
   id = id-1;
-  console.log("id: " + id);
   let score = 0;
+  let sumOfWeights = 0;
   for (let aspect of aspects) {
-    let values = [];
     if (aspect.weight) {
+      let values = [];
+      sumOfWeights += aspect.weight;
       for (let alternative of alternatives)
         values.push(alternative.values[aspect.id]);
-      let relativeValue = 0;
-      const min = Math.min(...values);
-      const max = Math.max(...values);
-      if (aspect.best === "max") {
-        relativeValue = (values[id]) / max;
-      } else if (aspect.best === "min") {
-        relativeValue = min/(values[id]);
-      }
-      console.log(aspect.id + relativeValue*aspect.weight);
+      const relativeValue = calcRelativeValue(values, aspect.best, values[id]);
       score += relativeValue*aspect.weight;
     }
   }
-  return score;
+  console.log(calcRelativeValue([1, 2, 3], "min", 2));
+  return Math.round(score/sumOfWeights*10);
 };
 
 export default calcScore;
